0730d7cb87228936c485ba26a0735471f35dba38
Ernesto Ittig <e.ittig@gmail.com>
2019-06-17 19:36:36 -0300

    This no longer sucks
    YOu can now call '% -1 4'

--- %/%.c
+++ %/%.c
@@ -1,8 +1,8 @@
 /*gcc -std=c99 -Wall "%.c" -lm -o "../bin/%/%"*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
-#include <getopt.h>
 
 long modE(long D, long d) {
     long r = D%d;
@@ -20,20 +20,25 @@ long modF(long D, long d) {
 }
 
 int main(int argc, char *const *argv) {
-    int opt, isArgs = 0, final;
-    while ((opt = getopt(argc, argv, ":tfe")) != -1) {
-        isArgs = 1;
-        final = opt;
-        if (opt == '?') {
-            if (optopt > 47 && optopt < 58) 
-            printf("unknow option %c\n", optopt);
-            exit(2);
-        }
-    }
-    
     if (argc < 3) {
         fprintf(stderr, "Two arguments are needed\n");
         exit(2);
+    }
+    
+    int isArgs = 0, optind = 1, final;
+
+    int nOpts = 3;
+    char opts[] = {'t', 'f', 'e'};
+
+    for (int i = 0; i < nOpts; i++) {
+        const char _buf[] = {'-', opts[i], 0};
+        const char *buf = _buf;
+        if (strcmp(buf, argv[1]) == 0) {
+            isArgs = 1;
+            final = opts[i];
+            optind = 2;
+            break;
+        }
     }
 
     long a = atoi(argv[optind]);
